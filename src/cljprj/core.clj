1dce981c5ea3ffcb3cb6a7d84261417dce9004d4
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-08-24 21:02:29 +0100

    new lein project

---
+++ src/cljprj/core.clj
@@ -0,0 +1,1 @@
+(ns cljprj.core)
---
+++ test/cljprj/test/core.clj
@@ -0,0 +1,6 @@
+(ns cljprj.test.core
+  (:use [cljprj.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
