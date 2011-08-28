edf957519fe571d09d47587fc88fc8f9f115981b
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-08-28 20:17:19 +0100

    docstrings, refactoring

--- src/cljprj/core.clj
+++ src/cljprj/core.clj
@@ -1,18 +1,22 @@
 (ns cljprj.core
   (:require [cljprj.persistence :as db]))
 
-(defn valid-project? [prj-data]
+(defn valid-project?
+  "Checks if the project contains the minimal amount of data to be valid"
+  [prj-data]
   (and
     (map? prj-data)
-    (not (nil? (prj-data :name)))
-    (not (nil? (prj-data :group-id)))
-    (not (nil? (prj-data :artifact-id)))))
+    (contains? prj-data :name)
+    (contains? prj-data :group-id)
+    (contains? prj-data :artifact-id)))
 
-(defn make-location [prj]
+(defn make-location-url
+  "Makes the URL for a single project"
+  [prj]
   (str "/api/projects/" (prj :group-id) "/" (prj :artifact-id)))
 
 (defn- coords
-  "Create the value to be used in the get-field for mongo.
+  "Create the value to be used in the mongo index.
    As get is always by gid/aid we just need those two things."
   [gid aid]
   (str gid " $$$ " aid))
@@ -25,7 +29,7 @@
     (do
       (db/add-project {:project prj :coords-idx (prj-coords prj)})
       {:status 201
-       :headers {"location" (make-location prj)}
+       :headers {"location" (make-location-url prj)}
        :body {:message "yum, thanks"}})
     {:status 400
      :body {:error "uploaded project must have at least [:name :group-id :artifact-id]"}}))
@@ -33,20 +37,26 @@
 
 (def no-such-project {:status 404 :body {:error "No such project"}})
 
-(defn get-project [gid aid]
+(defn get-project
+  "retrieves a single project"
+  [gid aid]
   (let [result (db/get-project (coords gid aid))]
     (if result
       {:body result}
       no-such-project)))
 
-(defn rm-project [gid aid]
+(defn rm-project
+  "removes a project from the service"
+  [gid aid]
   (let [result (db/rm-project (coords gid aid))]
     (if result
       {:status 204 :body nil}
       no-such-project)))
 
-(defn attach-href [prj]
-  (assoc prj :href (make-location prj)))
+(defn attach-href
+  "Adds the :href attribute to a project"
+  [prj]
+  (assoc prj :href (make-location-url prj)))
 
 (defn list-projects []
   (let [results (db/list-projects)]
