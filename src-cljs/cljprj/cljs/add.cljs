681c9535c3f9a0b43e513e234e9a162fdfaf54e2
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-08-29 14:11:01 +0100

    Added behaviour for when projects are submitted

--- src-cljs/cljprj/cljs/add.cljs
+++ src-cljs/cljprj/cljs/add.cljs
@@ -18,6 +18,9 @@
 (defn dom-val [elem-id]
   (.value (dom/getElement elem-id)))
 
+(defn clear-elem [elem-id]
+  (set! (.value (dom/getElement elem-id)) ""))
+
 (def whitespace-regex (js* "/\\s+/"))
 
 (def upload-headers
@@ -25,14 +28,38 @@
     (aset "Content-Type" "application/clojure")
     (aset "Accept" "application/clojure")))
 
+;;;;;;;;; SHOW AN INDIVIDUAL PROJECT BUSINESS
+
+(defn load-individual-project-from [locn]
+  (log (str "Loading from " locn)))
+
 
 ;;;;;;;;; UPLOAD A NEW PROJECT BUSINESS
+
+(defn clear-add-form []
+  (doall
+    (map
+      clear-elem
+      ["add-name" "add-group-id" "add-artifact-id" "add-author"
+       "add-latest-version" "add-source-url" "add-readme-text" "add-tags"])))
+
+(defn project-upload-success [new-locn]
+  (clear-add-form)
+  (load-individual-project-from new-locn))
 
 (defn submit-callback [e]
   (let [resp (aget e "target")
         status (. resp (getStatus))
-        body (. resp (getResponseText))]
-    (log [:submit-response status body])))
+        body (. resp (getResponseText))
+        location (.getResponseHeader resp "location")]
+
+    (log [:submit-response status body])
+    (log location)
+
+    (condp = status
+      201 (project-upload-success location)
+      400 (log "Upload failed :(")
+      (log "Unexpected problem with upload"))))
 
 (defn project-from-form []
   {:name (dom-val "add-name")
