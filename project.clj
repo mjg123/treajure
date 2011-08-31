88da6d2353a2d9474b45d4b36e827adbd2af86e2
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-08-31 20:31:09 +0100

    updated description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject cljprj "1.0.0-SNAPSHOT"
-  :description "Rooomy meeting room planner"
+  :description "treajure - a clojure library service"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "0.6.4"]
